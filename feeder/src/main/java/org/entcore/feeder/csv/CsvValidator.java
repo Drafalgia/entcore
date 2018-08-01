rdjedjig/entcore-mirror

[Evo] CAV2-161 : Intermediate storage system for manual corrections during import.

feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -43,12 +43,11 @@ import static org.entcore.feeder.csv.CsvFeeder.generateUserExternalId;
 import static org.entcore.feeder.csv.CsvFeeder.relativeStudentMapping;
 import static org.entcore.feeder.utils.CSVUtil.getCsvReader;
 
-public class CsvValidator extends Report implements ImportValidator {
+public class CsvValidator extends CsvReport implements ImportValidator {
 
 	private enum CsvValidationProcessType { VALIDATE, COLUMN_MAPPING }
 	private final Vertx vertx;
 	private String structureId;
-	private final ProfileColumnsMapper columnsMapper;
 	private final MappingFinder mappingFinder;
 	private boolean findUsersEnabled = true;
 	private final Map<String, String> classesNamesMapping = new HashMap<>();
@@ -64,10 +63,8 @@ public class CsvValidator extends Report implements ImportValidator {
 		profiles = Collections.unmodifiableMap(p);
 	}
 
-	public CsvValidator(Vertx vertx, String acceptLanguage, JsonObject customMapping) {
-		super(acceptLanguage);
-		setMappings(customMapping);
-		this.columnsMapper = new ProfileColumnsMapper(customMapping);
+	public CsvValidator(Vertx vertx, JsonObject importInfos) {
+		super(vertx, importInfos);
 		this.mappingFinder = new MappingFinder(vertx);
 		this.vertx = vertx;
 	}
@@ -77,8 +74,43 @@ public class CsvValidator extends Report implements ImportValidator {
 	}
 
 	@Override
+	public void validate(final Handler<JsonObject> handler) {
+		exportFiles(new Handler<AsyncResult<String>>() {
+			@Override
+			public void handle(AsyncResult<String> event) {
+				if (event.succeeded()) {
+					validate(event.result(), handler);
+				} else {
+					addError(event.cause().getMessage());
+					handler.handle(result);
+				}
+			}
+		});
+	}
+
+	@Override
 	public void validate(final String p, final Handler<JsonObject> handler) {
 		process(p, CsvValidationProcessType.VALIDATE, handler);
+	}
+
+	@Override
+	public boolean isValid() {
+		return !containsErrors();
+	}
+
+	@Override
+	public void exportIfValid(final Handler<JsonObject> handler) {
+		if (isValid()) {
+			exportFiles(new Handler<AsyncResult<String>>() {
+				@Override
+				public void handle(AsyncResult<String> event) {
+					if (event.failed()) {
+						addError(event.cause().getMessage());
+					}
+					handler.handle(result);
+				}
+			});
+		}
 	}
 
 	private void process(final String p, final CsvValidationProcessType processType, final Handler<JsonObject>  handler) {
@@ -184,6 +216,7 @@ public class CsvValidator extends Report implements ImportValidator {
 			int i = 0;
 			while ((strings = csvParser.readNext()) != null) {
 				if (i == 0) {
+					addHeader(profile, new JsonArray(strings));
 					JsonArray invalidColumns = columnsMapper.getColumsNames(profile, strings, columns);
 					if (invalidColumns.size() > 0) {
 						parseErrors("invalid.column", invalidColumns, profile, handler);
@@ -298,13 +331,15 @@ public class CsvValidator extends Report implements ImportValidator {
 					handler.handle(result);
 					return;
 				}
+//				setStructureExternalIdIfAbsent(structure.getExternalId());
 				try {
 					CSVReader csvParser = getCsvReader(path, charset, 1);
 					String[] strings;
-					int i = 0;
+					int i = 1;
 					while ((strings = csvParser.readNext()) != null) {
 						final JsonArray classesNames = new JsonArray();
 						JsonObject user = new JsonObject();
+						JsonArray linkStudents = new JsonArray();
 						user.putArray("structures", new JsonArray().add(structure.getExternalId()));
 						user.putArray("profiles", new JsonArray().add(profile));
 						List<String[]> classes = new ArrayList<>();
@@ -407,7 +442,6 @@ public class CsvValidator extends Report implements ImportValidator {
 						}
 						switch (profile) {
 							case "Relative":
-								JsonArray linkStudents = new JsonArray();
 								user.putArray("linkStudents", linkStudents);
 								for (String attr : user.getFieldNames()) {
 									if ("childExternalId".equals(attr)) {
@@ -481,15 +515,16 @@ public class CsvValidator extends Report implements ImportValidator {
 										}
 									}
 								}
-								for (Object o : linkStudents) {
-									if (!(o instanceof String)) continue;
-									if (classesNamesMapping.get(o) != null) {
-										classesNames.addString(classesNamesMapping.get(o));
+								for (Object obj : linkStudents) {
+									if (!(obj instanceof String)) continue;
+									final String s = classesNamesMapping.get((String) obj);
+									if (s != null) {
+										classesNames.addString(s);
 									}
 								}
 								break;
 						}
-						String error = validator.validate(user, acceptLanguage);
+						String error = validator.validate(user, acceptLanguage, true);
 						if (error != null) {
 							log.warn(error);
 							addError(profile, error);
@@ -498,7 +533,10 @@ public class CsvValidator extends Report implements ImportValidator {
 							classesNamesMapping.put(user.getString("externalId"), classesStr);
 							addUser(profile, user.putString("state", translate(state.name()))
 									.putString("translatedProfile", translate(profile))
-									.putString("classesStr", classesStr));
+									.putString("classesStr", classesStr)
+									.putArray("childExternalId", linkStudents)
+									.putNumber("line", i + 1)
+							);
 						}
 						i++;
 					}

feeder/src/main/java/org/entcore/feeder/csv/ProfileColumnsMapper.java
@@ -45,7 +45,7 @@ public class ProfileColumnsMapper {
 				.putString("id", "externalId")
 				.putString("externalid", "externalId")
 				.putString("nom", "lastName")
-				.putString("nomusage", "username")
+				.putString("nomusage", "surname")
 				.putString("prenom", "firstName")
 				.putString("classe", "classes")
 				.putString("datedenaissance", "birthDate")
@@ -74,7 +74,7 @@ public class ProfileColumnsMapper {
 				.putString("ignore", "ignore");
 		JsonObject studentMappings = baseMappings.copy()
 				.putString("nomeleve", "lastName")
-				.putString("nomdusageeleve", "username")
+				.putString("nomdusageeleve", "surname")
 				.putString("prenomeleve", "firstName")
 				.putString("niveau", "level")
 				.putString("regime", "accommodation")
@@ -90,7 +90,7 @@ public class ProfileColumnsMapper {
 				.putString("persreleleve", "relative");
 		JsonObject relativeMapping = baseMappings.copy()
 				.putString("nomresponsable", "lastName")
-				.putString("nomusageresponsable", "username")
+				.putString("nomusageresponsable", "surname")
 				.putString("prenomresponsable", "firstName")
 				.putString("idenfant", "childExternalId")
 				.putString("childid", "childExternalId")

Drafalgia/entcore

feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -46,12 +46,11 @@ import static org.entcore.feeder.csv.CsvFeeder.*;
 import static org.entcore.feeder.utils.CSVUtil.emptyLine;
 import static org.entcore.feeder.utils.CSVUtil.getCsvReader;
 
-public class CsvValidator extends Report implements ImportValidator {
+public class CsvValidator extends CsvReport implements ImportValidator {
 
 	private enum CsvValidationProcessType { VALIDATE, COLUMN_MAPPING }
 	private final Vertx vertx;
 	private String structureId;
-	private final ProfileColumnsMapper columnsMapper;
 	private final MappingFinder mappingFinder;
 	private boolean findUsersEnabled = true;
 	private final Map<String, String> classesNamesMapping = new HashMap<>();
@@ -69,10 +68,8 @@ public class CsvValidator extends Report implements ImportValidator {
 		profiles = Collections.unmodifiableMap(p);
 	}
 
-	public CsvValidator(Vertx vertx, String acceptLanguage, JsonObject customMapping) {
-		super(acceptLanguage);
-		setMappings(customMapping);
-		this.columnsMapper = new ProfileColumnsMapper(customMapping);
+	public CsvValidator(Vertx vertx, JsonObject importInfos) {
+		super(vertx, importInfos);
 		this.mappingFinder = new MappingFinder(vertx);
 		this.vertx = vertx;
 		defaultStudentSeed = new Random().nextLong();
@@ -83,8 +80,43 @@ public class CsvValidator extends Report implements ImportValidator {
 	}
 
 	@Override
+	public void validate(final Handler<JsonObject> handler) {
+		exportFiles(new Handler<AsyncResult<String>>() {
+			@Override
+			public void handle(AsyncResult<String> event) {
+				if (event.succeeded()) {
+					validate(event.result(), handler);
+				} else {
+					addError(event.cause().getMessage());
+					handler.handle(result);
+				}
+			}
+		});
+	}
+
+	@Override
 	public void validate(final String p, final Handler<JsonObject> handler) {
 		process(p, CsvValidationProcessType.VALIDATE, handler);
+	}
+
+	@Override
+	public boolean isValid() {
+		return !containsErrors();
+	}
+
+	@Override
+	public void exportIfValid(final Handler<JsonObject> handler) {
+		if (isValid()) {
+			exportFiles(new Handler<AsyncResult<String>>() {
+				@Override
+				public void handle(AsyncResult<String> event) {
+					if (event.failed()) {
+						addError(event.cause().getMessage());
+					}
+					handler.handle(result);
+				}
+			});
+		}
 	}
 
 	private void process(final String p, final CsvValidationProcessType processType, final Handler<JsonObject>  handler) {
@@ -190,6 +222,7 @@ public class CsvValidator extends Report implements ImportValidator {
 			int i = 0;
 			while ((strings = csvParser.readNext()) != null) {
 				if (i == 0) {
+					addHeader(profile, new JsonArray(strings));
 					JsonArray invalidColumns = columnsMapper.getColumsNames(profile, strings, columns);
 					if (invalidColumns.size() > 0) {
 						parseErrors("invalid.column", invalidColumns, profile, handler);
@@ -306,6 +339,7 @@ public class CsvValidator extends Report implements ImportValidator {
 					return;
 				}
 				final JsonObject checkChildExists = new JsonObject();
+//				setStructureExternalIdIfAbsent(structure.getExternalId());
 				try {
 					CSVReader csvParser = getCsvReader(path, charset, 1);
 					final int nbColumns = columns.size();
@@ -325,6 +359,7 @@ public class CsvValidator extends Report implements ImportValidator {
 //						}
 						final JsonArray classesNames = new fr.wseduc.webutils.collections.JsonArray();
 						JsonObject user = new JsonObject();
+						JsonArray linkStudents = new JsonArray();
 						user.put("structures", new fr.wseduc.webutils.collections.JsonArray().add(structure.getExternalId()));
 						user.put("profiles", new fr.wseduc.webutils.collections.JsonArray().add(profile));
 						List<String[]> classes = new ArrayList<>();
@@ -552,7 +587,7 @@ public class CsvValidator extends Report implements ImportValidator {
 								}
 								break;
 						}
-						String error = validator.validate(user, acceptLanguage);
+						String error = validator.validate(user, acceptLanguage, true);
 						if (error != null) {
 							log.warn(error);
 							addErrorByFile(profile, "validator.errorWithLine", "" + (i+1), error); // Note that 'error' is already translated
@@ -563,7 +598,10 @@ public class CsvValidator extends Report implements ImportValidator {
 							}
 							addUser(profile, user.put("state", translate(state.name()))
 									.put("translatedProfile", translate(profile))
-									.put("classesStr", classesStr));
+									.put("classesStr", classesStr)
+									.put("childExternalId", linkStudents)
+									.put("line", i + 1)
+							);
 						}
 						i++;
 					}

feeder/src/main/java/org/entcore/feeder/csv/ProfileColumnsMapper.java
@@ -55,6 +55,7 @@ public class ProfileColumnsMapper {
 				.put("nomusageresponsable", "username")
 				.put("nomusage", "username")
 				.put("nomdusage", "username")
+				.put("nomusage", "surname")
 				.put("prenom", "firstName")
 				.put("nombre", "firstName")
 				.put("firstname", "firstName")
@@ -157,7 +158,7 @@ public class ProfileColumnsMapper {
 				.put("ignore", "ignore");
 		JsonObject studentMappings = baseMappings.copy()
 				.put("nomeleve", "lastName")
-				.put("nomdusageeleve", "username")
+				.put("nomdusageeleve", "surname")
 				.put("prenomeleve", "firstName")
 				.put("niveau", "level")
 				.put("regime", "accommodation")
@@ -175,7 +176,7 @@ public class ProfileColumnsMapper {
 				.put("persreleleve", "relative");
 		JsonObject relativeMapping = baseMappings.copy()
 				.put("nomresponsable", "lastName")
-				.put("nomusageresponsable", "username")
+				.put("nomusageresponsable", "surname")
 				.put("prenomresponsable", "firstName")
 				.put("idenfant", "childExternalId")
 				.put("childid", "childExternalId")